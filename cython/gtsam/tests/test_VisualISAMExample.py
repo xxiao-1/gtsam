acdf02ec9c7f185090d07cf1f32370f835c3f797
Varun Agrawal <varagrawal@gmail.com>
2020-02-03 16:50:32 -0500

    added test for full ISAM2 update method

--- cython/gtsam/tests/test_VisualISAMExample.py
+++ cython/gtsam/tests/test_VisualISAMExample.py
@@ -6,8 +6,10 @@ All Rights Reserved
 See LICENSE for the license information
 
 visual_isam unit tests.
-Author: Frank Dellaert & Duy Nguyen Ta (Python)
+Author: Frank Dellaert & Duy Nguyen Ta & Varun Agrawal (Python)
 """
+# pylint: disable=maybe-no-member,invalid-name
+
 import unittest
 
 import numpy as np
@@ -52,6 +54,52 @@ class TestVisualISAMExample(GtsamTestCase):
         for j in range(len(truth.points)):
             point_j = result.atPoint3(symbol(ord('l'), j))
             self.gtsamAssertEquals(point_j, truth.points[j], 1e-5)
+
+    def test_isam2_update(self):
+        """
+        Test for full version of ISAM2::update method
+        """
+        # Data Options
+        options = generator.Options()
+        options.triangle = False
+        options.nrCameras = 20
+
+        # iSAM Options
+        isamOptions = visual_isam.Options()
+        isamOptions.hardConstraint = False
+        isamOptions.pointPriors = False
+        isamOptions.batchInitialization = True
+        isamOptions.reorderInterval = 10
+        isamOptions.alwaysRelinearize = False
+
+        # Generate data
+        data, truth = generator.generate_data(options)
+
+        # Initialize iSAM with the first pose and points
+        isam, result, nextPose = visual_isam.initialize(data, truth, isamOptions)
+
+        remove_factor_indices = gtsam.FactorIndices()
+        constrained_keys = gtsam.KeyGroupMap()
+        no_relin_keys = gtsam.KeyList()
+        extra_reelim_keys = gtsam.KeyList()
+        isamArgs = (remove_factor_indices,
+                    constrained_keys,
+                    no_relin_keys,
+                    extra_reelim_keys,
+                    False)
+
+        # Main loop for iSAM: stepping through all poses
+        for currentPose in range(nextPose, options.nrCameras):
+            isam, result = visual_isam.step(data, isam, result, truth, currentPose, isamArgs)
+
+        for i in range(len(truth.cameras)):
+            pose_i = result.atPose3(symbol(ord('x'), i))
+            self.gtsamAssertEquals(pose_i, truth.cameras[i].pose(), 1e-5)
+
+        for j in range(len(truth.points)):
+            point_j = result.atPoint3(symbol(ord('l'), j))
+            self.gtsamAssertEquals(point_j, truth.points[j], 1e-5)
+
 
 if __name__ == "__main__":
     unittest.main()
